da34e30c83eceac83d2bde6b10d8bf7673d46975
Moonkaman <nickbatmanglidj@gmail.com>
2019-02-21 16:45:44 -0700

    Add last MVP endpoint to recipies which is a post endpoint

--- recipes/recipesRouter.js
+++ recipes/recipesRouter.js
@@ -12,4 +12,14 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json({errorMessage: 'Could not retrieve the list of recipes at this time', error: err}));
 })
 
+router.post('/', (req, res) => {
+  if(req.body.name){
+    db.add(table, req.body)
+      .then(newRecipe => res.status(200).json(newRecipe))
+      .catch(err => res.status(500).json({errorMessage: 'Could not create a recipe at this time', error: err}));
+  } else {
+    res.status(400).json({errorMessage: `Please provide a name with your request`});
+  }
+})
+
 module.exports = router;
